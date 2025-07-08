6ec214b9ea18d50d966b0abf5d0cac394f9beee8
Ahmed <as3992054@gmail.com>
2025-07-08 18:58:31 +0300

    Implement Demo book class and handle its no deliver service

--- DemoBook.java
+++ DemoBook.java
@@ -1,2 +1,10 @@
 public class DemoBook extends Book {
+    public DemoBook(String isbn, String title, int publicationYear, float price) {
+        super(isbn, title, publicationYear, price, new NoDeliveryService());
+    }
+    
+    @Override
+    public boolean isForSale() {
+        return false;
+    }
 }
---
+++ NoDeliveryService.java
@@ -0,0 +1,16 @@
+/*
+    NoDeliveryService is a concrete implementation of the DeliveryService interface
+    that represents the absence of delivery logic (e.g., for demo or showcase books).
+
+    This class follows the Null Object Pattern, providing a safe default behavior
+    instead of using null or conditional checks.
+
+    To not break the strategy and more easily extensible code.
+ */
+
+public class NoDeliveryService implements DeliveryService {
+    @Override
+    public void deliver(Book book, String destination) {
+        System.out.println("Quantum book store - '" + book.title + "' is not for Sale.");
+    }
+}
