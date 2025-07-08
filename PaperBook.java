ded8b19519b857860bba1a1e3b480ec3f3bde561
Ahmed <as3992054@gmail.com>
2025-07-08 19:03:35 +0300

    Implement Paper book class

--- PaperBook.java
+++ PaperBook.java
@@ -1,3 +1,12 @@
 public class PaperBook extends Book {
+    private int stock;
 
+    public PaperBook(String isbn, String title, int publicationYear, float price, int stock) {
+        super(isbn, title, publicationYear, price, new ShippingService());
+        this.stock = stock;
+    }
+
+    public int getStock() {
+        return stock;
+    }
 }
