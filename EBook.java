3121344b6189c6f4b661ed7d64b3a8509cc822bd
Ahmed <as3992054@gmail.com>
2025-07-08 19:02:06 +0300

    Implement EBook class

--- EBook.java
+++ EBook.java
@@ -1,2 +1,12 @@
 public class EBook extends Book {
+    private String fileType;
+
+    public EBook(String isbn, String title, int publicationYear, float price, String fileType) {
+        super(isbn, title, publicationYear, price, new MailService());
+        this.fileType = fileType;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
 }
